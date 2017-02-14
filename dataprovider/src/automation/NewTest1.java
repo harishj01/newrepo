78af312aff3a4e0152fae59ff597f35501ad875f
harishj01 <harishj0119@gmail.com>
2017-02-14 17:23:29 +0530

    committed new repo

---
+++ dataprovider/src/automation/NewTest1.java
@@ -0,0 +1,26 @@
+package automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+	
+	WebDriver driver;
+@BeforeTest
+ 	 public void openbrowser()
+ 	 {
+	System.setProperty("webdriver.gecko.driver","C:\\Users\\Admin\\Downloads\\geckodriver-v0.14.0-win32\\geckodriver.exe");
+    driver = new FirefoxDriver();
+ 	 }
+	
+  @Test
+  public void testjenkins() {
+	  
+	  
+	  System.out.println("welcome to Jenkins");
+	  
+	  System.out.println("test");
+  }
+}
